76ce0bdeefb312bbcd61cdfef46446a749508813
AAsriyan <aasriyan@protonmail.com>
2019-03-18 14:30:32 -0400

    Set up dependencies/initial commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // implement your API here
+const express = require("express");
+
+const db = require("./data/db.js");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(4000, () => {
+  console.log("\n** API up and running on port 4000 **");
+});
